07a3cdd022a0deb77adecf0aa45cef3a89b24109
M.Hammad <hammmadakhter@gmail.com>
2023-10-06 18:00:57 +0500

    Ecommerce-website
    Ecommerce-website using react

---
+++ Ecommerce-Web/src/components/CardsData.js
@@ -0,0 +1,112 @@
+const Cardsdata = [
+    {
+        id: 1,
+        rname: "Mens Shirts",
+        imgdata: "https://www.stoneharbor.com.pk/cdn/shop/products/stone-harbor-men-s-tee-shirt-blue-s-men-s-yarn-dyed-panels-tee-shirt-31525579915317_5000x.jpg?v=1649936791",
+        address: "Ecommerce-website,karachi",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1175 + order placed from here recently",
+        price: 1350,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+        qnty:0
+    },
+    {
+        id: 2,
+        rname: "Mens Jeans",
+        imgdata: "https://static-01.daraz.pk/p/1e381b643f511bb86db0a938ef60bf57.jpg",
+        address: "Ecommerce-website,karachi",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 2525 + order placed from here recently",
+        price: 2350,
+        rating: "3.9",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 3,
+        rname: "Mens Shoes",
+        imgdata: "https://ae01.alicdn.com/kf/Sa2627256ff29469e920f25254cef78afG/Men-s-summer-slip-on-casual-flat-socks-shoes-new-fashion-breathable-knitted-mesh-sneakers-shock.jpg",
+        address: "Ecommerce-website,karachi",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 650 + order placed from here recently",
+        price: 3450,
+        rating: "4.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 4,
+        rname: "Mens Watch",
+        imgdata: "https://static-01.daraz.pk/p/d6712fa144965814920dc8d9b37d38d3.jpg",
+        address: "Ecommerce-website,karachi",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 300 + order placed from here recently",
+        price: 3150,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 5,
+        rname: "Ladies Shirts",
+        imgdata: "https://www.alamodelabel.in/cdn/shop/products/IMG-20230420-WA0002_600x.jpg?v=1682075122",
+        address: "Ecommerce-website,karachi",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "1050 + order placed from here recently",
+        price: 1210,
+        rating: "4.0",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 6,
+        rname: "Ladies Jeans",
+        imgdata: "https://brandcollection.com.pk/cdn/shop/products/1_15a2f365-0fb2-4cfc-9d87-e4974a7ddaab_1024x1024.jpg?v=1645202044",
+        address: "Ecommerce-website,karachi",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: " 1100 + order placed from here recently",
+        price: 1100,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 7,
+        rname: "Ladies Watch",
+        imgdata: "https://static-01.daraz.pk/p/516ff78db72845bef7b154b557a5f1af.jpg",
+        address: "Ecommerce-website,karachi",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 2300,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 8,
+        rname: "Ladies Purse",
+        imgdata: "https://images.meesho.com/images/products/157222813/mtqas_512.webp",
+        address: "Ecommerce-website,karachi",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "500 + order placed from here recently",
+        price: 1250,
+        rating: "3.2",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+    {
+        id: 9,
+        rname: "Ladies Shoes",
+        imgdata: "https://dressfair.pk/image/cache/catalog/product-2280/black-casual-slip-on-thick-platform-ladies-shoes-x8x8BEfNv8-550x550.jpeg",
+        address: "Ecommerce-website,karachi",
+        delimg: "https://b.zmtcdn.com/data/o2_assets/0b07ef18234c6fdf9365ad1c274ae0631612687510.png?output-format=webp",
+        somedata: "2525 + order placed from here recently",
+        price: 1400,
+        rating: "3.8",
+        arrimg: "https://b.zmtcdn.com/data/o2_assets/4bf016f32f05d26242cea342f30d47a31595763089.png?output-format=webp",
+         qnty:0
+    },
+];
+
+export default Cardsdata;
